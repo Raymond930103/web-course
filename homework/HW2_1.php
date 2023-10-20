d0c78a65f06c56b756998f2c90af55a00269bee2
raymond_mac <raymond2040103@gmail.com>
2023-10-21 00:45:45 +0800

    correct

--- homework/HW2_1.php
+++ homework/HW2_1.php
@@ -1,19 +1,18 @@
 <?php
 define("USD_to_TWD", 30);
 define("Pi" , 3.14159);
-
+define("egg", 500);
 
 $usd = 2500;
 $twd = $usd * USD_to_TWD;
 
 $radius = 25;
-$area = 2*$radius*Pi;
+$area = 2 * $radius *Pi;
 
-$egg = 500;
-$dozen = floor($eggs/12);
-$left /= $eggs;
+$dozen = floor(egg / 12);
+$left = egg % 12;
 
-echo "1. {$usd}美金換成新臺幣為{$twd}新臺幣<br>";
+echo "1. {$usd}美金換成新臺幣約為{$twd}新臺幣<br>";
 echo "2. 半徑長{$radius}的圓面積為{$area}<br>";
-echo "3. {$egg}顆蛋有{$dozen}打,還剩{$left}";
+echo "3. ",egg,"顆蛋有{$dozen}打還剩{$left}顆<br>";
 ?>
